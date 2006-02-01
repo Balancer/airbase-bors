f348a24bcbd3a53e5c16d58d8a955ee53d33eb62
balancer <devnull@localhost>
2006-02-01 10:46:05 +0000

    Ресурсы смарти - файлы.

---
+++ funcs/templates/smarty-register.php
@@ -0,0 +1,8 @@
+<?
+    require_once("smarty-resource-file.php");
+
+    $smarty->register_resource("xfile", array("smarty_resource_file_get_template",
+                                       "smarty_resource_file_get_timestamp",
+                                       "smarty_resource_file_get_secure",
+                                       "smarty_resource_file_get_trusted"));
+?>
---
+++ funcs/templates/smarty-resource-file.php
@@ -0,0 +1,44 @@
+<?
+    // from PHP script
+    // put these function somewhere in your application
+
+    function smarty_resource_file_get_template($tpl_name, &$tpl_source, &$smarty_obj)
+    {
+        // do database call here to fetch your template,
+        // populating $tpl_source
+		if(file_exists($tpl_name))
+		{
+			$tpl_source = ec(file_get_contents($tpl_name));
+			return true;
+		}
+        else 
+        {
+            return false;
+        }
+    }
+    
+    function smarty_resource_file_get_timestamp($tpl_name, &$tpl_timestamp, &$smarty_obj)
+    {
+		if(file_exists($tpl_name))
+		{
+			$tpl_timestamp = filemtime($tpl_name);
+			return true;
+		}
+        else 
+        {
+            return false;
+        }
+    }
+
+    function smarty_resource_file_get_secure($tpl_name, &$smarty_obj)
+    {
+        // assume all templates are secure
+        return true;
+    }
+
+    function smarty_resource_file_get_trusted($tpl_name, &$smarty_obj)
+    {
+        // not used for templates
+    }
+
+?>
