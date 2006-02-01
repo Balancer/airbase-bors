1c56be4168d25829705e55c7506b260d6c38a8da
balancer <devnull@localhost>
2006-02-01 10:46:00 +0000

    модуль навигации в стиле Plone.

---
+++ modules/show/childs-row.inc.php
@@ -0,0 +1,21 @@
+<?
+    function module_show_childs_row($parent_uri)
+    {
+		$hts = new DataBaseHTS();
+		$parent_uri = $hts->normalize_uri($parent_uri);
+		$data = array();
+		$links = array();
+
+		foreach($hts->dbh->get_array("
+				SELECT c.value as uri, t.value as title
+				FROM `hts_data_child` c
+					LEFT JOIN `hts_data_title` t ON c.id = t.id
+				WHERE c.id LIKE '".addslashes($parent_uri)."'", false) as $link)
+			$links[] = $link;
+
+		$data['links'] = $links;
+
+		include_once("funcs/templates/assign.php");
+		return template_assign_data("xfile:".dirname(__FILE__)."/childs-row.htm", $data);
+    }
+?>
---
+++ modules/show/childs-row.php
@@ -0,0 +1,4 @@
+<?
+	include_once("childs-row.inc.php");
+    echo module_show_childs_row($GLOBALS['module_data']['root']);
+?>
