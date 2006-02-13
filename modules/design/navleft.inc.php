3837580354425d4efae75dc05e0483f0cfcc5e23
balancer <devnull@localhost>
2006-02-13 09:29:02 +0000

    Исправления в работе модуля показа навигации в левой колонке.

--- modules/design/navleft.inc.php
+++ modules/design/navleft.inc.php
@@ -2,60 +2,93 @@
 	function modules_design_navleft_get($uri)
 	{
 		include_once("funcs/templates/assign.php");
-		return template_assign_data("xfile:".dirname(__FILE__)."/navleft.htm", array('links'=>modules_design_navleft($uri, NULL, 0)));
+		$data = modules_design_navleft($uri, array(), 1);
+		$max = 0;
+
+		foreach($data as $d)
+			if($d['indent'] > $max)
+				$max = $d['indent'];
+
+		foreach($data as $d)
+		{
+			$d['indent'] = $max - $d['indent'];
+			$data2[] = $d;
+		}
+		
+		return template_assign_data("xfile:".dirname(__FILE__)."/navleft.htm", array('links'=>$data2));
 	}
+
+	$GLOBALS['loop'] = 0;
 	
 	function modules_design_navleft($uri, $data, $indent)
 	{
-		$hts = new DataBaseHTS();
+		$GLOBALS['loop']++;
 		
+		if($GLOBALS['loop'] > 50)
+			return $data;
+		
+		if(!empty($data[$uri]))
+			return $data;
+		
+		$hts = new DataBaseHTS();
+
+		$children = $hts->get_data_array($uri, 'child');
+		
+		$out = array();
+		$out['uri'] = array(
+				'indent' => $indent,
+				'uri' => $uri,
+				'title' => $hts->get_data($uri, 'nav_name'),
+				'children' => sizeof($children)
+			);
+
 		if(!$data)
 		{
-			$data[] = array(
-					'indent'=>$indent,
-					'uri'=>'',
-					'title'=>$hts->get_data($uri, 'nav_name'),
-					'children'=>sizeof($hts->get_data_array($uri, 'child'))
-				);
-			$GLOBALS['navleft-parent-seen'] = array();
-			if($hts->get_data_array($uri, 'child'))
+			if($children)
 			{
 				foreach($hts->dbh->get_array("
 					SELECT ch.value as uri,
-							nn.value as title,
-							".intval($indent+1)." as indent
+							nn.value as title
 						FROM hts_data_child ch
 							LEFT JOIN hts_data_nav_name nn ON nn.id = ch.value
 						WHERE ch.id = '".addslashes($uri)."'
 							AND nn.id IS NOT NULL
 						GROUP BY ch.value
 						ORDER BY nn.value") as $u)
-					$data[] = $u + array('children'=>sizeof($hts->get_data_array($u['uri'],'child')));
+				{
+					$data[$u['uri']] = $u + array(
+							'children' => sizeof($hts->get_data_array($u['uri'],'child')),
+							'indent' => $indent-1
+						);
+				}
 			}
 		}
-					
+
+		$data = array_merge($out, $data);
+
 		$parent = $hts->get_data($uri, 'parent');
+
+//		echo "<p><b>Parent for $uri = $parent</b></p>";
+//		print_r($data); echo "<br/>";
 		
-		if(!$parent || !empty($GLOBALS['navleft-parent-seen'][$parent]))
+		if(!$parent || !empty($data[$parent]))
 			return $data;
+
+		$indent++;		
 
 		$out = array();
 		if($hts->get_data_array($parent, 'parent'))
-			$out = array(
-					'indent'=>$indent, 
+			$out[$parent] = array(
+					'indent' => $indent, 
 					'uri' => $parent, 
-					'title'=>$hts->get_data($parent, 'nav_name'),
-					'children'=>sizeof($hts->get_data_array($parent, 'child'))
+					'title' => $hts->get_data($parent, 'nav_name'),
+					'children' => sizeof($hts->get_data_array($parent, 'child'))
 				);
 
-		$indent++;		
-			
-		$GLOBALS['navleft-parent-seen'][$parent] = true;
-		
 		foreach($hts->dbh->get_array("
 			SELECT ch.value as uri,
 					nn.value as title,
-					".intval($indent)." as indent
+					".intval($indent-1)." as indent
 				FROM hts_data_child ch
 					LEFT JOIN hts_data_nav_name nn ON nn.id = ch.value
 				WHERE ch.id = '".addslashes($parent)."'
@@ -64,11 +97,16 @@
 				ORDER BY nn.value
 			") as $u)
 		{
-			if($u['uri'] == $uri)
-				$out += $data;
-			else
-				$out[] = $u + array('children'=>sizeof($hts->get_data_array($u['uri'],'child')));
+//			echo "<p>Before:</p><xmp>"; print_r($out); echo "</xmp>;u=".(print_r($u,true))."<br/>";
+			{
+				if($u['uri'] == $uri)
+					$out = array_merge($out, $data);
+				else
+					$out[$u['uri']] = $u + array('children'=>sizeof($hts->get_data_array($u['uri'],'child')));
+			}
+//			echo "After:<br/><xmp>"; print_r($out); echo "</xmp><br/>";
 		}
+
 		return modules_design_navleft($parent, $out, $indent);
 	}
 ?>
