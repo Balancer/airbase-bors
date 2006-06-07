d2cc09e05cb6c679ef0164a491d3fb76d416bf82
Balancer <devnull@localhost>
2006-06-07 12:06:01 +0000

    Up from 1001kran

--- admin/tools/.rename.php
+++ admin/tools/.rename.php
@@ -1,12 +1,12 @@
-#!/usr/bin/php
+#!/usr/local/bin/php
 <?
-	$_SERVER['DOCUMENT_ROOT'] = '/var/www/bal.aviaport.ru/htdocs';
+	$_SERVER['DOCUMENT_ROOT'] = '/home/kra61882/public_html';
 	
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/cms/config.php");
 	require_once("funcs/DataBaseHTS.php");
 
 	$hts = new DataBaseHTS();
 
-	$hts->rename_host('http://www.aviaport.ru/forum-new', 'http://bal.aviaport.ru/conferences');
-//	$hts->rename_host('/conference/', '/conferences/');
+//	$hts->rename_host('http://www.aviaport.ru/forum-new', 'http://bal.aviaport.ru/conferences');
+	$hts->rename_host('http://1001kran.runews/', 'http://1001kran.ru/news/');
 ?>
