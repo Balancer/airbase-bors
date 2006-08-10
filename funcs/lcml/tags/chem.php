1c2d4fb17f6dbfe3ec5d55ef596590d3cd2c9eac
balancer <devnull@localhost>
2006-08-10 08:00:05 +0000

    Ряд новых тэгов, в т.ч. WikiTeX и т.п.

---
+++ funcs/lcml/tags/chem.php
@@ -0,0 +1,14 @@
+<?
+    function lp_chem($text,$params)
+    {
+		global $wgScriptPath;
+		error_reporting( E_ALL & ~ E_NOTICE );
+		define( 'MEDIAWIKI', true );
+		$incs =  ini_get('include_path');
+		include_once("/var/www/wiki.airbase.ru/htdocs/LocalSettings.php");
+		$ret = Wikitex::chem($text, array());
+		error_reporting( E_ALL );
+		ini_set('include_path', $incs);
+		return $ret;
+	}
+?>
