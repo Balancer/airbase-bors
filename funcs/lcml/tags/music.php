5397d5a3752944cd5277fce1c80dec223fd9f16e
balancer <devnull@localhost>
2006-08-10 08:00:05 +0000

    Ряд новых тэгов, в т.ч. WikiTeX и т.п.

---
+++ funcs/lcml/tags/music.php
@@ -0,0 +1,17 @@
+<?
+    function lp_music($text,$params)
+    {
+		global $wgScriptPath;
+		error_reporting( E_ALL & ~ E_NOTICE );
+		define( 'MEDIAWIKI', true );
+		$incs =  ini_get('include_path');
+		include_once("/var/www/wiki.airbase.ru/htdocs/LocalSettings.php");
+		
+		$text = html_entity_decode($text, ENT_NOQUOTES);
+
+		$ret = Wikitex::music($text, array());
+		error_reporting( E_ALL );
+		ini_set('include_path', $incs);
+		return $ret;
+	}
+?>
