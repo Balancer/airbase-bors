9c6347ce3ba6ccbb3ff1650d2a1b680290de216b
balancer <devnull@localhost>
2006-11-07 08:51:02 +0000

    Репутация и предупреждения для форума.

---
+++ plugins/users/handlers/reputation/reputation_add.php
@@ -0,0 +1,71 @@
+<?
+    register_action('reputation_add_do', 'plugins_users_reputation_add_do');
+
+    function plugins_users_reputation_add_do($uri, $action)
+	{
+		require_once('funcs/modules/messages.php');
+
+		$uid = intval($_POST['user_id']);
+		if(!$uid)
+			return error_message(ec("Не задан ID пользователя."));
+
+		$me = &new User();
+		$dbf = &new DataBase('punbb');
+		$dbu = &new DataBase('USERS');
+		$me_id = $me->get('id');
+		
+		if($me_id == 1)
+			return error_message(ec("Голосование возможно только для авторизованных пользователей."));
+
+		if($me_id == $uid)
+			return error_message(ec("Нельзя ставить репутацию самому себе."));
+		
+		if($dbf->get("SELECT num_posts FROM users WHERE id=$me_id") < 50)
+			return error_message(ec("Репутацию выставлять могут только участники, имеющие более 50 сообщений на форуме."));
+
+		$dbu->insert('reputation_votes', array(
+			'user_id'		=> $uid,
+			'time'			=> time(),
+			'score'			=> $_POST['score'] > 0 ? 1 : -1,
+			'voter_id'		=> $me_id,
+			'uri'			=> $_POST['uri'],
+			'comment'		=> $_POST['comment'],
+		));
+
+		$grw = array(
+				1 => 8, // admin
+				2 => 6, // moder
+				3 => 0, // guest
+				5 => 4, // coordin
+				6 => 2, // старожилы
+			);
+
+		$total = 0;
+		foreach($dbu->get_array("SELECT voter_id as id, SUM(score) as sum FROM `reputation_votes` WHERE user_id = $uid GROUP BY voter_id") as $v)
+		{
+			$reput = (atan($dbf->get("SELECT reputation FROM users WHERE id={$v['id']}"))*2/pi() + 1)/2;
+			$group = $dbf->get("SELECT group_id FROM users WHERE id={$v['id']}");
+				
+			$weight = @$grw[$group];
+			if(!$weight)
+				$weight = 1;
+
+			if($v['id'] == 10000)
+				$weight = 10;
+					
+			if($dbf->get("SELECT num_posts FROM users WHERE id={$v['id']}") < 50)
+				$weight = 0;
+				
+			$sum = atan($v['sum'])*2/pi() * $weight * $reput;
+			$total += $sum;
+		}
+
+		$dbf->query("UPDATE users SET reputation = '".str_replace(",",".",$total)."' WHERE id = $uid");
+
+		include_once("funcs/Cache.php");
+		$ch = new Cache();
+		$ch->clear_by_id("http://balancer.ru/user/$uid/avatarblock/");
+
+		include_once("funcs/navigation/go.php");
+		go("http://balancer.ru/user/$uid/reputation/");
+	}
---
+++ plugins/users/handlers/warnings/show.php
@@ -0,0 +1,53 @@
+<?
+	hts_data_prehandler("()(\d+)/warnings/", array(
+			'body'		=> 'plugins_users_warn_show_body',
+			'title'		=> 'plugins_users_warn_show_title',
+			'nav_name'	=> 'plugins_users_warn_show_nav_name',
+			'template'	=> "{$_SERVER['DOCUMENT_ROOT']}/cms/templates/forum/forum.html",
+		));
+
+	function plugins_users_warn_show_body($uri, $m)
+	{
+		require_once('funcs/modules/messages.php');
+
+		$uid = $data['user_id']	= intval($m[2]);
+		if(!$uid)
+			return ec("Не задан ID пользователя.");
+
+		$db = new DataBase('punbb');
+		
+		$data['ref'] = urldecode(@$_GET['ref']) or @$_SERVER['HTTP_REFERER'];
+		
+		$data['active_warnings'] = $db->get_array("SELECT * FROM warnings WHERE user_id = $uid AND time > ".(time()-86400*30)." ORDER BY time DESC");
+		$data['passive_warnings'] = $db->get_array("SELECT * FROM warnings WHERE user_id = $uid AND time < ".(time()-86400*30)." ORDER BY time DESC");
+
+        include_once("funcs/templates/assign.php");
+        return template_assign_data("show.html", $data);
+	}
+
+	function plugins_users_warn_show_title($uri, $m)
+	{
+		require_once('funcs/modules/messages.php');
+
+		$uid = $data['user_id']	= intval($m[2]);
+		if(!$uid)
+			return ec("Не задан ID пользователя.");
+
+		$us = new User($uid);
+
+		return ec("Штрафы пользователя ") . $us->data('name');
+	}
+
+	function plugins_users_warn_show_nav_name($uri, $m)
+	{
+		require_once('funcs/modules/messages.php');
+
+		$uid = $data['user_id']	= intval($m[2]);
+		if(!$uid)
+			return ec("Не задан ID пользователя.");
+
+		$us = new User($uid);
+
+		return $us->data('name');
+	}
+?>
