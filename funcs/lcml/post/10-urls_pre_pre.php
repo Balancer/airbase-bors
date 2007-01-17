20329abb3ca409528857638f396a720865c1633d
Balancer <devnull@localhost>
2007-01-17 14:55:04 +0000

    Ещё забыл это.

---
+++ funcs/lcml/post/10-urls_pre_pre.php
@@ -0,0 +1,9 @@
+<?
+    function lcml_urls_pre_pre($txt)
+    {
+        $txt = preg_replace("!\[url\](.+?)\[/url\]!is", "<a href=\"$1\">$1</a>", $txt);
+        $txt = preg_replace("!\[url=(.+?)\](.+?)\[/url\]!is", "<a href=\"$1\">$2</a>", $txt);
+        $txt = preg_replace("!\[img=(.+?)\]!is", "<img src=\"$1\" />", $txt);
+
+        return $txt;
+    }
