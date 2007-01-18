db014c2d934c56794ea1d0f09b8579c31dd5dc93
Balancer <devnull@localhost>
2007-01-18 08:38:05 +0000

    Исправления тэга [url]....[/url]

--- funcs/lcml/post/10-urls_post.php
+++ funcs/lcml/post/10-urls_post.php
@@ -1,7 +1,6 @@
 <?
-    function lcml_urls_pre_pre($txt)
+    function lcml_urls_post($txt)
     {
-        $txt = preg_replace("!\[url\](.+?)\[/url\]!is", "<a href=\"$1\">$1</a>", $txt);
         $txt = preg_replace("!\[url=(.+?)\](.+?)\[/url\]!is", "<a href=\"$1\">$2</a>", $txt);
         $txt = preg_replace("!\[img=(.+?)\]!is", "<img src=\"$1\" />", $txt);
 
