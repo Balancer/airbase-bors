f5358b3bc80b96bb106ea0196ab3c901325ff288
Balancer <devnull@localhost>
2007-08-29 08:32:03 +0000

    Небольшой рефакторинг персонального списка на форуме.

---
+++ classes/bors/forum/js/personal.php
@@ -0,0 +1,31 @@
+<?
+class_include("def_js");
+
+class forum_js_personal extends def_js
+{
+	var $owner;
+
+	function forum_js_personal($id)
+	{
+		parent::def_js($id);
+		$this->owner = class_load('forum_user', $this->id());
+	}
+
+	
+	function _class_file() { return __FILE__; }
+		
+	function cache_static()
+	{
+		return 7*86400;
+	}
+		
+	function url()
+	{
+		return "http://balancer.ru/user/".$this->id()."/personal.js";
+	}
+
+	function owner()
+	{
+		return $this->owner;
+	}
+}
