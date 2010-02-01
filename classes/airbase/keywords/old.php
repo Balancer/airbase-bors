0fa5a2543b8a67394d8877a8199cc44a8f460bca
Balancer <balancer@balancer.ru>
2010-02-02 02:13:04 +0300

    Поддержка старых ключевых слов.

---
+++ classes/airbase/keywords/old.php
@@ -0,0 +1,34 @@
+<?php
+
+class airbase_keywords_old extends base_object
+{
+	private $url = false;
+
+	function can_be_empty() { return false; }
+	static function id_prepare($id) { return trim(trim(urldecode($id)), '/'); }
+
+	function loaded()
+	{
+		$found = false;
+		$kw = $this->id();
+		foreach(file($_SERVER['DOCUMENT_ROOT'].'/links.txt') as $s)
+		{
+			if(preg_match('/^\s+/', $s))
+			{
+				if($found)
+					return $this->url = trim($s);
+
+				continue;
+			}
+			if(trim($s) == $kw)
+				$found = true;
+		}
+
+		return false;
+	}
+
+	function pre_show()
+	{
+		return go($this->url);
+	}
+}
--- handlers/bors_map.php
+++ handlers/bors_map.php
@@ -16,6 +16,7 @@ $map = array(
 
 	'(/)forums/? => base_page_redirect(NULL,go=http://forums.airbase.ru)',
 	'(/)cgi\-bin/forum/ultimatebb\.cgi\?ubb=forum&f=(\d+) => redirect:forum_forum(2)',
+	'(/)cgi\-bin/forum/ultimatebb\.cgi\?f=(\d+)&ubb=forum => redirect:forum_forum(2)',
 
 	'(/)forum/? => base_page_redirect(NULL,go=http://forums.airbase.ru)',
 
@@ -30,4 +31,7 @@ $map = array(
 //	'.*viewtopic\.php\?id=(\d+).* => forum_topic(1)',
 //	'(.*) => base_page_hts(1,host=0)',
 //	'(.*) => airbase_page(1,host=0)',
+
+	'/([^/]+)/ => airbase_keywords_old(1)',
+	'/([^/]+) => airbase_keywords_old(1)',
 );
