1f16fcf43b0e6d17eeb4d6c17fda5355ab76c94f
Balancer <balancer@balancer.ru>
2010-06-15 07:56:32 +0400

    ++

---
+++ sites/la2.balancer.ru/config.php
@@ -0,0 +1,19 @@
+<?php
+
+config_set('default_template', 'bors:http://la2.balancer.ru/cms/templates/default/');
+config_set('sitemap_classes', 'balancer_board_topic');
+
+register_vhost('airbase.ru', NULL, '/var/www/airbase.ru/.bors-host');
+register_vhost('airbase.ru:8080', '/var/lib/tomcat-6/webapps');
+register_vhost('balancer.ru', NULL, '/var/www/balancer.ru/.bors-host');
+register_vhost('bionco.ru', NULL, '/var/www/bionco.ru/bors-site');
+register_vhost('files.balancer.ru', '/var/www/files.balancer.ru/files');
+register_vhost('forums.airbase.ru', NULL, '/var/www/forums.airbase.ru/.bors-host');
+register_vhost('forums.balancer.ru', NULL, '/var/www/forums.balancer.ru/bors-host');
+register_vhost('games.balancer.ru');
+register_vhost('la2.balancer.ru', NULL, '/var/www/la2.balancer.ru/.bors-host');
+register_vhost('la2.wrk.ru');
+register_vhost('mail.balancer.ru');
+register_vhost('navy.balancer.ru', NULL, '/var/www/navy.balancer.ru/bors-host');
+register_vhost('top.airbase.ru');
+register_vhost('wrk.ru');
