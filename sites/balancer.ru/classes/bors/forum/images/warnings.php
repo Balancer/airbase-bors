d1ab81a14f7497a8dc6080bf976879445ae6ce67
Balancer <balancer@balancer.ru>
2010-09-02 00:43:39 +0400

    Индикация административного бана.

--- sites/balancer.ru/classes/bors/forum/images/warnings.php
+++ sites/balancer.ru/classes/bors/forum/images/warnings.php
@@ -4,12 +4,19 @@ class forum_images_warnings extends base_image_gif
 {
 	private $expired = NULL;
 
+	function auto_objects()
+	{
+		return array(
+			'user' => 'balancer_board_user(id)',
+		);
+	}
+
     function image()
 	{
 		$image_name = "skull"; // "cross"
-	
+
 		$user_id = $this->id();
-			
+
 		if($user_id)
 		{
 			$db = &new DataBase('punbb');
@@ -20,26 +27,26 @@ class forum_images_warnings extends base_image_gif
 
 		$func1 = "imagecreatefromgif";
 		$func2 = "imagegif";
-			
+
 		$ww = 100;
 		$hh = 16;
 
 		$img = imagecreatetruecolor($ww, $hh);
-		
+
 		$transparent = imagecolorallocate($img, 255,99,140);
 	    imagecolortransparent($img, $transparent);
 
 		imagefill($img, 0, 0, $transparent);
-		
+
 		$cross_full = imagecreatefromgif("/var/www/balancer.ru/htdocs/img/web/{$image_name}.gif");
 		$cross_half = imagecreatefromgif("/var/www/balancer.ru/htdocs/img/web/{$image_name}-half.gif");
-		
+
 		$sx = imagesx($cross_full);
 		$sy = imagesy($cross_full);
 
 		$cross_full_count = intval($warn_count / 2);
 		$cross_half_count = $warn_count % 2;
-		
+
 		for($i=0; $i<$cross_full_count; $i++)
 			imagecopy($img, $cross_full, 10+$i*$sx, 0, 0, 0, $sx, $sy);
 
@@ -48,7 +55,8 @@ class forum_images_warnings extends base_image_gif
 
 //		$warn_count = 10; // -----------------
 
-		if($warn_count >= 10)
+		$user = $this->user();
+		if($warn_count >= 10 || $user->is_banned())
 		{
 			$total = 0;
 			$time  = 0;
@@ -67,7 +75,10 @@ class forum_images_warnings extends base_image_gif
 			$black = imagecolorallocate($img,   0,   0,   0);
 			$white = imagecolorallocate($img, 255, 255, 255);
 
-			$text = ec('бан до '.strftime("%d.%m.%Y", $this->expired = $time+WARNING_DAYS*86400));
+			if($user->is_banned())
+				$text = ec('        админбан    ');
+			else
+				$text = ec('бан до '.strftime("%d.%m.%Y", $this->expired = $time+WARNING_DAYS*86400));
 //			$text = ec('бан до '.strftime("%d.%m.%Y", time() + rand(3*86400, 365*10*86400)));
 
 			$x = 0;
@@ -91,13 +102,13 @@ class forum_images_warnings extends base_image_gif
 		imagedestroy($img);
 		imagedestroy($cross_full);
 		imagedestroy($cross_half);
-			
+
 		return $result;
 	}
-	
+
 	function url() { return "http://balancer.ru/user/{$this->id()}/warnings.gif"; }
-	
+
 	function cache_static() { return $this->expired ? $this->expired - time() : 86400; }
-	
+
 	function cache_groups() { return "user-{$this->id()}-warnings"; }
 }
