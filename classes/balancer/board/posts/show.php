3a704ae36530d2de1d73b3628ef79667201b693a
Balancer <balancer@balancer.ru>
2010-09-10 17:25:01 +0400

    Новый просмотр постингов

---
+++ classes/balancer/board/posts/show.php
@@ -0,0 +1,30 @@
+<?php
+
+class balancer_board_posts_show extends bors_page
+{
+	function config_class() { return 'balancer_board_config'; }
+
+	function parents() { return array($this->post()->topic()->url()); }
+
+	function auto_objects()
+	{
+		return array(
+			'post' => 'balancer_board_post(id)',
+		);
+	}
+
+	function title() { return $this->post()->title(); }
+	function nav_name() { return $this->post()->nav_name(); }
+	function description() { return $this->post()->description(); }
+
+	function local_data()
+	{
+		return array(
+			'post' => $this->post(),
+			'topic' => $this->post()->topic(),
+			'forum' => $this->post()->topic()->forum(),
+		);
+	}
+
+	function url() { return $this->post()->url(); }
+}
