7863dc02e63f948c574a355c2aa11f8156dd783d
Balancer <balancer@balancer.ru>
2010-09-19 09:21:41 +0400

    ++

--- classes/bors/user/js/touch.php
+++ classes/bors/user/js/touch.php
@@ -3,19 +3,17 @@
 class user_js_touch extends base_page
 {
 	function template() { return 'empty.html'; }
-	
+
 	function init()
 	{
-		@header("Content-type", "text/javascript");
-		@header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-		@header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+		template_nocache();
 
 		$this->set_loaded();
-	
+
 		$obj = object_load($this->id());
 		if(!$obj || !bors()->user())
 			return;
-		
+
 		$obj->touch(bors()->user()->id());
 	}
 
