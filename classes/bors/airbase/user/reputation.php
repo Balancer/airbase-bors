5a74787b31d763d84bb0a66ec5d0e3cdfb5afe19
Balancer <balancer@balancer.ru>
2010-09-22 08:38:52 +0400

    Небольшой фикс отображения репутации.

--- classes/bors/airbase/user/reputation.php
+++ classes/bors/airbase/user/reputation.php
@@ -26,4 +26,29 @@ class airbase_user_reputation extends base_page_db
 	function titled_url() { return $this->title(); }
 
 	function cache_groups_parent() { return "user-{$this->user_id()}-reputation"; }
+
+	function refer_link()
+	{
+		$ref = $this->refer();
+		if(preg_match('/^\w+__\d+$/', $ref))
+		{
+			$object = object_load($ref);
+			return object_property($object, 'titled_url');
+		}
+
+		if(preg_match('!post://(\d+)/?!', $ref, $m))
+		{
+			$object = object_load('balancer_board_post', $m[1]);
+			return object_property($object, 'titled_url');
+		}
+
+		$object = object_load($ref);
+		if(is_a($object, 'bors_system_go_internal'))
+			$object = $object->target();
+
+		if($object)
+			return $object->titled_url();
+
+		return $ref;
+	}
 }
