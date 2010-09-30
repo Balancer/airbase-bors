614c3900dc20952ba8c4993eed59df4ea90a1f7f
Balancer <balancer@balancer.ru>
2010-09-30 10:30:28 +0400

    Статические картинки, начало поддержки премодификаций сообщений, перевод статики на отдельный домен.

--- classes/balancer/board/admin/main.php
+++ classes/balancer/board/admin/main.php
@@ -5,4 +5,15 @@ class balancer_board_admin_main extends base_page
 	function title() { return ec('Управление форумами'); }
 	function nav_name() { return ec('управление'); }
 	function config_class() { return 'balancer_board_admin_config'; }
+
+	function local_data()
+	{
+		$files = glob("/data/backup/punbb-*.sql.gz");
+		function rsort_by_mtime($file1, $file2) { return filemtime($file2) - filemtime($file1); }
+		usort($files,"rsort_by_mtime");
+
+		return array_merge(parent::local_data(), array(
+			'last_punbb_backup_mtime' => filemtime($files[0]),
+		));
+	}
 }
