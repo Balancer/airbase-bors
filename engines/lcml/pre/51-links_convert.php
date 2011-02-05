0ba945c6389bfc05203f14f96295b25322647396
Balancer <balancer@balancer.ru>
2011-02-05 18:00:25 +0300

    Ошибочка.

--- engines/lcml/pre/51-links_convert.php
+++ engines/lcml/pre/51-links_convert.php
@@ -3,7 +3,7 @@
 function lcml_links_convert($text)
 {
 	// Android Market
-	$text = preg_replace('!market://search\?q=pname:(\S+)!', '[android_market]$1[/android_market]', $text);
-	$text = preg_replace('!https://market\.android\.com/details\?id=(\S+)!', '[android_market]$1[/android_market]', $text);
+	$text = preg_replace('!(^|\s)market://search\?q=pname:(\S+)!m', '\1[android_market]\2[/android_market]', $text);
+	$text = preg_replace('!(^|\s)https://market\.android\.com/details\?id=(\S+)!m', '\1[android_market]\2[/android_market]', $text);
 	return $text;
 }
