28ce3774efc58218a9c32ba272b7908a5b776414
Balancer <balancer@balancer.ru>
2011-02-07 18:32:48 +0300

    Доработка гуглеридер-ретранслятора.

--- engines/lcml/pre/51-links_convert.php
+++ engines/lcml/pre/51-links_convert.php
@@ -5,5 +5,9 @@ function lcml_links_convert($text)
 	// Android Market
 	$text = preg_replace('!(^|\s)market://search\?q=pname:(\S+)!m', '\1[android_market]\2[/android_market]', $text);
 	$text = preg_replace('!(^|\s)https://market\.android\.com/details\?id=(\S+)!m', '\1[android_market]\2[/android_market]', $text);
+
+	// Android market images
+	$text = preg_replace('!(^|\s)(https://ssl.gstatic.com/android/market/[^/]+/ss\S+)!m', '\1[img \2]', $text);
+
 	return $text;
 }
--- sites/balancer.ru/classes/balancer/retranslate/googlereader.php
+++ sites/balancer.ru/classes/balancer/retranslate/googlereader.php
@@ -4,30 +4,21 @@ class balancer_retranslate_googlereader extends base_page
 {
 	function render_engine() { return __CLASS__; }
 	function is_auto_url_mapped_class() { return true; }
+	function can_be_empty() { return true; }
+	function loaded() { return true; }
 
 	function render()
 	{
 		// ispired from http://www.mezzoblue.com/archives/2008/12/11/authenticati/
 
-		// ----------------------------------------
-		// Google Reader Authentication in PHP
-		// a basic script to get you in the door of 
-		// Google's unofficial Reader API
-		// by Dave Shea, mezzoblue.com
-		// ----------------------------------------
-
-		// cobbled together from notes on:
-		// http://code.google.com/p/pyrfeed/wiki/GoogleReaderAPI
-
-		// these are the urls we'll need to access various services
-		$urlAtom = "http://www.google.com/reader/atom";
-
 		$ch = curl_init('https://www.google.com/accounts/ClientLogin');
 
-		$data = array('accountType' => 'GOOGLE',
+		$data = array(
+			'accountType' => 'GOOGLE',
 			'Email' => config('balabot.login.google'),
 			'Passwd' => config('balabot.password.google'),
-			'source'=>'BalaBot retranslator',
+//			'source'=>'BalaBot retranslator',
+			'source' => 'exampleCo-exampleApp-1',
 			'service'=>'reader',
 		);
 
@@ -45,44 +36,23 @@ class balancer_retranslate_googlereader extends base_page
 		$loginResult = curl_exec($ch);
 		curl_close ($ch);
 
-		var_dump($loginResult);
-		exit();
+//		var_dump($loginResult);
+//		exit();
 
-		// we just received three lines of ugliness to contend with.
-		// each line is a huge string preceded with an ID
-		// the IDs are: SID, LSID, and Auth; we only want SID
-		// let's use some string parsing to weed it out
-		if ($i = strstr($loginResult, "LSID"))
-		{
-			$SID = substr($loginResult, 0, (strlen($loginResult) - strlen($i)));
-			$SID = rtrim(substr($SID, 4, (strlen($SID) - 4)));
-		}
+		$authRaw = strstr($loginResult, "Auth");
+		$authToken = substr($authRaw, 5);
+		$user_id = '14299261987225235624';
+        $ch = curl_init("http://www.google.com/reader/atom/user/{$user_id}/state/com.google/broadcast");
+        $header[] = 'Authorization: GoogleLogin auth='.$authToken;
 
-		// so we've found the SID
-		// now we can build the cookie that gets us in the door
-		$cookie = "SID=" . $SID . "; domain=.google.com; path=/; expires=1600000000";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
 
-		// this builds the action we'd like the API to perform
-		// in this case, it's getting our list of unread items
-//		$action = $urlAtom . "/user/-/state/com.google/reading-list";
-		$action = $urlAtom . "/user/-/state/com.google/starred";
-		// note that the hyphen above is a shortcut
-		// for "the currently logged-in user"
+        $xml = curl_exec($ch);
+        curl_close($ch);
 
-		// start buffering what we get back
-		ob_start();
-		$ch = curl_init();
-		curl_setopt ($ch, CURLOPT_URL, $action);
-		curl_setopt ($ch, CURLOPT_HTTPGET, true);
-		curl_setopt ($ch, CURLOPT_COOKIE, $cookie);
-		curl_exec ($ch);
-		curl_close ($ch);
-		// throw the buffer into a variable
-		$xml = ob_get_contents();
-		ob_end_clean();
-
-		// and finally, let's take a look.
-//		header("Content-Type: ".$feed->contentType."; charset=".$feed->encoding);
-		echo $xml;
+		header("Content-Type: text/xml; charset=utf-8");
+		return $xml;
 	}
 }
