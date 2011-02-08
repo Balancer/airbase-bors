7f3892a0fd364b7f5c40b32da7863934d48ba693
Balancer <balancer@balancer.ru>
2011-02-08 14:29:53 +0300

    Первая реинкарнация Балабота.

--- classes/balancer/balabot.php
+++ classes/balancer/balabot.php
@@ -8,7 +8,56 @@ class balancer_balabot extends base_object
 
 	static function on_thumb_up($target)
 	{
-		debug_hidden_log('balabot', 'thumb_up: '.$target->debug_title());
+		debug_hidden_log('balabot', 'thumb_up: '.$target->debug_title(), false);
+
+		$bot = bors_find_first('balancer_board_user', array('title' => 'BalaBOT'));
+		//balancer_balabot::send_tweet('Ты скучен, Зойдберг.');
+
+		$twitter_class_id = class_name_to_id('bors_external_twitter');
+
+		$prev = bors_find_first('bors_users_blogs_map', array(
+			'target_class_id' => $target->extends_class_id(),
+			'target_object_id' => $target->id(),
+			'blog_class_id' => $twitter_class_id,
+		));
+
+		if($prev)
+			return; // Уже было размещено
+
+		$url = ' '.wrk_go::make_short_url($target);
+
+		$tpls = array(
+			array('Понравилось сообщение от %user% «', '»'),
+			array('%user% интересно пишет: ', ''),
+			array('Обратите внимание на пост %user% «', '»'),
+			array('Очень интересно: %user%> ', ''),
+		);
+
+		$tpl = $tpls[rand(0, count($tpls)-1)];
+
+		$pre  = $tpl[0];
+		$post = $tpl[1];
+
+		$pre  = str_replace('%user%', $target->owner()->title(), $pre); // $target->score()
+		$post = $post.$url;
+
+		$limit = 140 - bors_strlen($post) - bors_strlen($pre);
+		$text = strip_text($target->source(), $limit, '…', true);
+		$msg = $pre.$text.$post;
+
+		debug_hidden_log('balabot', "Limit=$limit, text lenth=".bors_strlen($text).", full length=".bors_strlen($msg).', text=|'.$msg.'|', false);
+
+		if($blog_object_id = bors_external_twitter::send($bot, $msg))
+		{
+			object_new_instance('bors_users_blogs_map', array(
+				'target_class_id' => $target->extends_class_id(),
+				'target_object_id' => $target->id(),
+				'blog_class_id' => $twitter_class_id,
+				'blog_object_id' => $blog_object_id,
+			));
+		}
+
+		debug_hidden_log('balabot', "{$target->debug_title()} posted in twitter as $blog_object_id", false);
 	}
 
 	static function send_tweet($msg)
@@ -30,8 +79,7 @@ class balancer_balabot extends base_object
 			$twitter->setOAuth($oauth);
 
 			$msg = $twitter->statuses->update($msg);
-
-//     print_r($msg);
+			print_r($msg);
 		}
 		catch (Services_Twitter_Exception $e)
 		{
