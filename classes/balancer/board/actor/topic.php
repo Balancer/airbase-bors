35378037cb02f669eb5bb010d473b2b3745d084b
Balancer <balancer@balancer.ru>
2011-03-17 01:22:21 +0300

    Дорабатываем ленту событий.

--- classes/balancer/board/actor/topic.php
+++ classes/balancer/board/actor/topic.php
@@ -15,7 +15,7 @@ class balancer_board_actor_topic extends bors_page
 		$post  = $this->args('object');
 		$user  = $this->args('user');
 		$topic = $post->topic();
-		return "В теме {$topic->titled_link_ex(array('page' => 'new'))} есть <a href=\"{$post->url_in_container()}\">новое сообщение</a> от пользователя {$post->owner()->titled_link()}";
+		return "Обновлена тема {$topic->titled_link_ex(array('page' => 'new'))}";
 	}
 
 	function public_text()
@@ -27,8 +27,10 @@ class balancer_board_actor_topic extends bors_page
 	function personal_text()
 	{
 		$post  = $this->args('object');
-		return strip_text($post->body(), 256);
+		$user  = $this->args('user');
+		$topic = $post->topic();
+		return "<b><a href=\"{$post->url_in_container()}\">{$post->owner()->title()}</a>: </b>".strip_text($post->body(), 256);
 	}
 
-	function color() { return NULL; }
+	function color() { return 'topic'; }
 }
