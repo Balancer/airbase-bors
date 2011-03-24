0d8dad02964ed3f242520b510ae6c88c5b11c2c0
Balancer <balancer@balancer.ru>
2011-03-24 17:36:35 +0300

    Карма.

--- classes/balancer/board/forum.php
+++ classes/balancer/board/forum.php
@@ -3,4 +3,13 @@
 class balancer_board_forum extends forum_forum
 {
 	function extends_class() { return 'forum_forum'; }
+
+	function last_topics($limit)
+	{
+		return bors_find_all('balancer_board_topic', array(
+			'forum_id' => $this->id(),
+			'order' => '-last_post_create_time',
+			'limit' => $limit,
+		));
+	}
 }
