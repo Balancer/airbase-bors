1172fab46d4f2290a4152469e0e216d91e313167
Balancer <balancer@balancer.ru>
2011-09-29 23:59:09 +0300

    Немного унификации.

---
+++ classes/balancer/board/paginated.php
@@ -0,0 +1,6 @@
+<?php
+
+class balancer_board_paginated extends bors_paginated
+{
+	function config_class() { return 'balancer_board_config'; }
+}
--- classes/bors/balancer/board/new/topics.php
+++ classes/bors/balancer/board/new/topics.php
@@ -1,10 +1,10 @@
 <?php
 
-class balancer_board_new_topics extends base_page_paged
+class balancer_board_new_topics extends balancer_board_paginated
 {
-	function main_class() { return 'forum_topic'; }
+	function main_class() { return 'balancer_board_topic'; }
 	function order() { return '-create_time'; }
-	function template() { return 'forum/_header.html'; }
+//	function template() { return 'forum/_header.html'; }
 	function title() { return 'Новые темы за месяц'; }
 	function nav_name() { return 'новые темы'; }
 	function where() { return array('posted>' => time()-86400*31, 'is_public' => 1); }
