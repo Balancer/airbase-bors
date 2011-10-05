c829dde1d20b1c7d9e200f8351211f7b3d2bd2a2
Balancer <balancer@balancer.ru>
2011-10-05 13:45:25 +0300

    Так красивее.

--- engines/lcml/pre/51-links_convert.php
+++ engines/lcml/pre/51-links_convert.php
@@ -3,8 +3,10 @@
 function lcml_links_convert($text)
 {
 	// Android Market
-	$text = preg_replace('!(^|\s)market://search\?q=pname:(\S+)!m', '\1[android_market]\2[/android_market]', $text);
-	$text = preg_replace('!(^|\s)https://market\.android\.com/details\?id=(\S+)!m', '\1[android_market]\2[/android_market]', $text);
+	$text = preg_replace('!^\s*https://market\.android\.com/details\?id=(\S+)\s*$!m', '\1[android_market display=block]\2[/android_market]', $text);
+	$text = preg_replace('!^\s*market://search\?q=pname:(\S+)\s*$!m', '\1[android_market display=block]\2[/android_market]', $text);
+	$text = preg_replace('!(^|\s)market://search\?q=pname:(\w+[\w\.]+\w+)!m', '\1[android_market display=inline]\2[/android_market]', $text);
+	$text = preg_replace('!(^|\s)https://market\.android\.com/details\?id=(\w+[\w\.]+\w+)!m', '\1[android_market display=inline]\2[/android_market]', $text);
 
 	// Android market images
 	$text = preg_replace('!(^|\s)(https://ssl.gstatic.com/android/market/[^/]+/ss\S+)!m', '\1[img \2]', $text);
--- engines/lcml/tags/adroid_market.php
+++ engines/lcml/tags/adroid_market.php
@@ -3,7 +3,20 @@
 function lp_android_market($market_id, $params)
 {
 	$market_url = "market://search?q={$market_id}";
-	$img_url = "http://qrcode.kaywa.com/img.php?s=4&d=".urlencode($market_url);
-	$img = "<img src=\"{$img_url}\" width=\"140\" height=\"140\"/>";
-	return "<a href=\"https://market.android.com/details?id={$market_id}\">{$img}</a>";
+	$url = "https://market.android.com/details?id={$market_id}";
+
+	if(defval($params, 'display') == 'block')
+	{
+		$img_url = "http://qrcode.kaywa.com/img.php?s=4&d=".urlencode($market_url);
+		$img = "<img src=\"{$img_url}\" width=\"140\" height=\"140\"/>";
+		return "<a href=\"{$url}\">{$img}</a>";
+	}
+
+	// inline
+
+	$data = bors_external_common::content_extract($url);
+//	if(config('is_developer'))
+//		var_dump($data);
+	$title = preg_replace('! - Android Маркет$!', '', defval($data, 'title', $market_id));
+	return "<a href=\"{$url}\">{$title}</a>";
 }
