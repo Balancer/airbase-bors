358a548f67173bb219d6164609a3f35b0c4b5b42
Balancer <balancer@balancer.ru>
2011-10-23 16:38:31 +0300

    ++

--- sites/admin.airbase.ru/classes/airbase/admin/config.php
+++ sites/admin.airbase.ru/classes/airbase/admin/config.php
@@ -5,7 +5,7 @@ class airbase_admin_config extends bors_admin_config
 	function config_data()
 	{
 		return array_merge(parent::config_data(), array(
-			'access_engine' => 'airbase_admin_access',
+			'access_engine' => 'airbase_access_balancer',
 		));
 	}
 }
