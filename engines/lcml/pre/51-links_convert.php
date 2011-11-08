e695c9000a7d18802eecd89b95b50f1b90cc6ede
Balancer <balancer@balancer.ru>
2011-11-08 15:43:14 +0300

    Улучшаем отображение ссылок на Маркет.

--- engines/lcml/pre/51-links_convert.php
+++ engines/lcml/pre/51-links_convert.php
@@ -3,8 +3,8 @@
 function lcml_links_convert($text)
 {
 	// Android Market
-	$text = preg_replace('!^\s*https://market\.android\.com/details\?id=(\S+)\s*$!m', '\1[android_market display=block]\2[/android_market]', $text);
-	$text = preg_replace('!^\s*market://search\?q=pname:(\S+)\s*$!m', '\1[android_market display=block]\2[/android_market]', $text);
+	$text = preg_replace('!^\s*https://market\.android\.com/details\?id=(\S+)\s*$!m', '[android_market display=block]\1[/android_market]', $text);
+	$text = preg_replace('!^\s*market://search\?q=pname:(\S+)\s*$!m', '[android_market display=block]\2[/android_market]', $text);
 	$text = preg_replace('!(^|\s)market://search\?q=pname:(\w+[\w\.]+\w+)!m', '\1[android_market display=inline]\2[/android_market]', $text);
 	$text = preg_replace('!(^|\s)https://market\.android\.com/details\?id=(\w+[\w\.]+\w+)!m', '\1[android_market display=inline]\2[/android_market]', $text);
 
--- engines/lcml/tags/adroid_market.php
+++ engines/lcml/tags/adroid_market.php
@@ -5,18 +5,20 @@ function lp_android_market($market_id, $params)
 	$market_url = "market://search?q={$market_id}";
 	$url = "https://market.android.com/details?id={$market_id}";
 
-	if(defval($params, 'display') == 'block')
-	{
-		$img_url = "http://qrcode.kaywa.com/img.php?s=4&d=".urlencode($market_url);
-		$img = "<img src=\"{$img_url}\" width=\"140\" height=\"140\"/>";
-		return "<a href=\"{$url}\">{$img}</a>";
-	}
-
-	// inline
-
 	$data = bors_external_common::content_extract($url);
 //	if(config('is_developer'))
 //		var_dump($data);
 	$title = preg_replace('! - Android Маркет$!', '', defval($data, 'title', $market_id));
+
+	if(defval($params, 'display') == 'block')
+	{
+		$img_url = "http://qrcode.kaywa.com/img.php?s=4&d=".urlencode($market_url);
+		$img = "<img src=\"{$img_url}\" width=\"140\" height=\"140\"/>";
+		$html = lcml($data['bbshort']);
+		return preg_replace("!^//(.+)$!m", "<div class=\"clear\">&nbsp;</div>\n<a href=\"{$url}\">{$img}</a></br>\n// $1", $html);
+	}
+
+	// inline
+
 	return "<a href=\"{$url}\">{$title}</a>";
 }
