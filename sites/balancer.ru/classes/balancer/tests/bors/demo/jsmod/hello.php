6eb3ef9abe3987f4c9899e0705b26e4b72ff4788
Balancer <balancer@balancer.ru>
2011-11-09 23:17:28 +0300

    Мелочь.

--- sites/balancer.ru/classes/balancer/tests/bors/demo/jsmod/hello.php
+++ sites/balancer.ru/classes/balancer/tests/bors/demo/jsmod/hello.php
@@ -4,8 +4,11 @@ class balancer_tests_bors_demo_jsmod_hello extends bors_module
 {
 	function body_data()
 	{
+		if(!($user = object_property(bors()->user(), 'title')))
+			$user = ec('Гость с IP=').bors()->client()->ip();
+
 		return array(
-			'guest_ip' => $this->get('mode') == 'static' ? 'xxx.xxx.xxx.xxx' : bors()->client()->ip(),
+			'guest' => $this->get('mode') == 'static' ? '' : $user,
 		);
 	}
 }
