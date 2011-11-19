61ebc9a3f80bdbfc53086e663f7b1803954f8f6d
Balancer <balancer@balancer.ru>
2011-11-19 06:20:37 +0300

    ++

---
+++ sites/airbase.ru/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//config_set('default_template', 'bors:http://airbase.ru/cms/templates/skins/default/body/');
+config_set('default_template', 'xfile:airbase/default/index2.html');
+//config_set('default_template', 'xfile:forum/common.html');
+
+config_set('sitemap_classes', 'balancer_board_topic');
+config_set('lcml_old_keywords', true);
+
+config_set('classes_auto_base', 'airbase');
