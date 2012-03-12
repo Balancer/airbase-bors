b215f7626e658db119929a703dd632f597d5451d
Balancer <balancer@balancer.ru>
2012-03-12 06:56:56 +0400

    ++

--- config-post.php
+++ config-post.php
@@ -2,11 +2,11 @@
 
 template_jquery();
 template_jquery_plugin('tageditor/jquery.tag.editor.js');
-template_jquery_plugin('jquery.lazyload-ad-1.4.min.js');
+template_jquery_plugin('jquery.lazyload-ad-1.4.min.js'); // Это загрузка с задержкой рекламных блоков!
 //template_jquery_plugin('jquery.lazyload.js'); — тормозит скроллинг
 
 template_js_include('/_bors/js/funcs3.js?');
-//template_js_include('/_bors/js/tune.js');
+template_js_include('/_bors/js/tune.js');
 template_js_include('/_bors/js/cfuncs.js');
 template_js_include('/_bal/js/common.js?');
 template_js_include('/_bors/js/bors-jquery.js');
