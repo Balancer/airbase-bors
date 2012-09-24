4dec8306a1d0ef312208e07af7949a302cbafaf0
Balancer <balancer@balancer.ru>
2012-09-25 01:21:08 +0400

    up

--- classes/bors/forum/user/js/setvars.php
+++ classes/bors/forum/user/js/setvars.php
@@ -2,7 +2,15 @@
 
 class forum_user_js_setvars extends bors_js
 {
-	function cache_static() { return config('static_forum') ? rand(3*86400, 7*86400) : 0; }
+/*
+	top.me_can_move=1
+	top.me_is_coordinator=1
+*/
+
+//	При переводе на статический форум сделать сброс по изменению свойств юзера. Или прав доступа. Разгрести.
+//	function cache_static() { return config('static_forum') ? rand(3*86400, 7*86400) : 0; }
+
+	function cache_static() { return rand(300, 600); }
 
 	function url() { return "/user/".$this->id()."/setvars.js"; }
 
