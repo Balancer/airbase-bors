4cdceaff27dd7753e731c0829f3383fed7165b39
Balancer <balancer@balancer.ru>
2012-09-29 11:45:22 +0400

    ++

--- sites/forums.balancer.ru/classes/balancer/board/stat/coordinators.php
+++ sites/forums.balancer.ru/classes/balancer/board/stat/coordinators.php
@@ -26,6 +26,24 @@ class balancer_board_stat_coordinators extends bors_page
 			'order' => 'warnings_count DESC',
 		));
 
-		return compact('month_warns', 'year_warns');
+		$month_actions = $dbh->select_array('board_actions', 'users.username as coordinator_name, count(*) as actions_count', array(
+			'inner_join' => 'users ON users.id = owner_id',
+			'message NOT LIKE "Предупреждение%"',
+			'create_time>' => time()-30.6*86400,
+			'group' => 'owner_id',
+			'having' => 'actions_count>0',
+			'order' => 'actions_count DESC',
+		));
+
+		$year_actions = $dbh->select_array('board_actions', 'users.username as coordinator_name, count(*) as actions_count', array(
+			'inner_join' => 'users ON users.id = owner_id',
+			'message NOT LIKE "Предупреждение%"',
+			'create_time>' => time()-365.24*86400,
+			'group' => 'owner_id',
+			'having' => 'actions_count>0',
+			'order' => 'actions_count DESC',
+		));
+
+		return compact('month_actions', 'month_warns', 'year_actions', 'year_warns');
 	}
 }
