d708650929466b3de7a0d2c72b91bf113ba0810c
Balancer <balancer@balancer.ru>
2012-10-31 05:55:47 +0400

    мелочи.

--- sites/balancer.ru/config-post.php
+++ sites/balancer.ru/config-post.php
@@ -1,4 +1,5 @@
 <?php
 
-template_js_include('/jquery-local/js/jquery-ui-1.8.16.custom.min.js');
-template_css('/jquery-local/css/smoothness/jquery-ui-1.8.16.custom.css');
+template_jquery_ui();
+//template_js_include('/jquery-local/js/jquery-ui-1.8.16.custom.min.js');
+//template_css('/jquery-local/css/smoothness/jquery-ui-1.8.16.custom.css');
