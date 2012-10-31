6acdec5951b725ad5f50cf901c1732568f822802
Balancer <balancer@balancer.ru>
2012-10-31 14:40:13 +0400

    Хм. Не было ещё DVCS? Инициализация!

---
+++ classes/tanzpol/main.php
@@ -0,0 +1,10 @@
+<?php
+
+class tanzpol_main extends tanzpol_page
+{
+	var $title_ec = 'Tanzpol';
+	var $nav_name_ec = 'политика';
+	var $description = 'Политические дискуссии';
+
+	function create_time() { return 1343759827; }
+}
