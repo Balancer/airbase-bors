85e946dd53276a9daf63e1d60150c40cd33eb64f
Balancer <balancer@balancer.ru>
2012-10-31 04:57:56 +0400

    Охренеть. Их не было o_O

---
+++ sites/balancer.ru/config-post.php
@@ -0,0 +1,4 @@
+<?php
+
+template_js_include('/jquery-local/js/jquery-ui-1.8.16.custom.min.js');
+template_css('/jquery-local/css/smoothness/jquery-ui-1.8.16.custom.css');
---
+++ sites/balancer.ru/config.php
@@ -0,0 +1,10 @@
+<?php
+
+config_set('default_template', 'blue_spring');
+
+config_set('sitemap_classes', 'balancer_board_topic');
+//config_set('cache_static', false); - чёрт, отключать, вроде как, и нельзя. Всякие звёздочки репутации и прочее...
+config_set('classes_auto_base', 'balancer');
+
+config_set('balabot.login.google', 'balabot@balancer.ru');
+config_set('balabot.password.google', 'balagupegu');
