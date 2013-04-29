33f09c38bbc413f86c6c8fd7e9a8eb037877b507
Balancer <balancer@balancer.ru>
2013-04-29 23:23:42 +0400

    Доработка страницы обновлённых тем.

---
+++ sites/forums.balancer.ru/classes/balancer/board/personal/updated.inc.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('.title_actions').each(function(){
+		t=$(this)
+		t.append('&nbsp;<img src="/_bors-ext/i16/actions/cancel.png" class="btn16 unwatch half-transparent" title="Перестать следить за темой" />')
+	})
+
+	$('.unwatch').live('click', function(){
+		t=$(this)
+		td = t.parent().parent()
+		topic_id = td.attr('id').replace('vtt_', '')
+		ans = confirm('Вы точно хотите перестать следить за обновлением этой темы?')
+		if(ans)
+			$.getScript('/_bors/tools/act/pub/balancer_board_ajax_actions_unwatch?topic_id='+topic_id)
+	})
+})
