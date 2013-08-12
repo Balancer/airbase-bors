1a9cf7205e88da87085c372746a26639ed4335f7
Balancer <balancer@balancer.ru>
2013-08-12 04:12:43 +0400

    Как это можно было забыть выложить? o_O

---
+++ classes/airbase/image.php
@@ -0,0 +1,12 @@
+<?php
+
+class airbase_image extends bors_image
+{
+//	function db_name() { return config('airbase.db'); }
+//	function table_name() { return 'images'; }
+//	function table_fields()
+//	{
+//		return array(
+//		);
+//	}
+}
