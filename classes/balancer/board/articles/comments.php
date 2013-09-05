a85ee9f268b49d3618d8607e00773b6174b8e5d2
Balancer <balancer@balancer.ru>
2013-09-06 01:23:07 +0400

    Что-то забытое.

---
+++ classes/balancer/board/articles/comments.php
@@ -0,0 +1,6 @@
+<?php
+
+class balancer_board_articles_comments extends balancer_board_topic
+{
+	function extends_class() { return 'balancer_board_topic'; }
+}
---
+++ classes/balancer/board/articles/list.php
@@ -0,0 +1,7 @@
+<?php
+
+class balancer_board_articles_list extends base_page_paged
+{
+	function main_class() { return 'balancer_board_articles_entry'; }
+	function class_file() { return __FILE__; }
+}
