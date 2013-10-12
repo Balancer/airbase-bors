148f9aaf9b099dd6236f46046cf6caefaab5232e
Balancer <balancer@balancer.ru>
2013-10-12 10:11:40 +0400

    Оптимизация списка обновлённых тем.

--- classes/airbase/image.php
+++ classes/airbase/image.php
@@ -14,4 +14,7 @@ class airbase_image extends bors_image
 	{
 //		if(empty)
 	}
+
+	function id_96x96() { return $this->id().',96x96(up,crop)'; }
+	function _thumbnail_96x96_def() { return $this->thumbnail('96x96(up,crop)'); }
 }
