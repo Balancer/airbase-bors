590fb34e6f50a8f519faf8a82b8877a9adb87383
Balancer <balancer@balancer.ru>
2013-12-26 07:34:01 +0400

    ++

--- classes/lcml/parser/airbase.php
+++ classes/lcml/parser/airbase.php
@@ -4,17 +4,21 @@ class lcml_parser_airbase extends bors_lcml_parser
 {
 	function html($text)
 	{
-		$text = preg_replace('!(^|\s)http://(www\.)?balancer\.ru/g/p(\d+)(\D)!mi', '$1[post]$3[/post]$4', $text);
-		$text = preg_replace('!(^|\s)http://forums\.airbase\.ru/\d+/\d+/[^/]+\.html?#p(\D)!mi', '$1[post]$3[/post]$4', $text);
-		$text = preg_replace('!(^|\s)http://(www\.)?balancer\.ru/(society/)?\d+/\d+/[^/]+\.html?#p(\D)!mi', '$1[post]$3[/post]$4', $text);
+		$text = preg_replace('!(^|\s)(http://(www\.)?balancer\.ru/g/p(\d+))(\D)!mi', '$1[post original="$2"]$4[/post]$5', $text);
+		$text = preg_replace('!(^|\s)(http://(forums\.airbase\.ru|.*balancer\.ru|.*wrk\.ru)/\d+/\d+/[^/]+\.html?#p(\d+))(\D)!mi', '$1[post original="$2"]$4[/post]$5', $text);
+		$text = preg_replace('!(^|\s)(http://(.*balancer\.ru|.*wrk\.ru)/(society/)?\d+/\d+/[^/]+\.html?#p(\d+))(\D)!mi', '$1[post original="$2"]$5[/post]$6', $text);
 
-		$text = preg_replace('!(^|\s)http://(www\.)?(balancer\.ru|forums\.airbase\.ru|wrk\/ru)\S*/\d{4}/\d{2}/t(\d+),(\d+)\S+\.html!mi', '$1[topic page=$5]$4[/topic]', $text);
-		$text = preg_replace('!(^|\s)http://(www\.)?(balancer\.ru|forums\.airbase\.ru|wrk\/ru)\S*/\d{4}/\d{2}/t(\d+)\S+\.html!mi', '$1[topic]$4[/topic]', $text);
+		$text = preg_replace('!(^|\s)(http://(.*balancer\.ru|forums\.airbase\.ru|.*wrk\.ru)\S*/\d{4}/\d{2}/t(\d+),(\d+)\S+\.html)!mi', '$1[topic page=$5 original="$2"]$4[/topic]', $text);
+		$text = preg_replace('!(^|\s)(http://(.*balancer\.ru|forums\.airbase\.ru|.*wrk\.ru)\S*/\d{4}/\d{2}/t(\d+)\S+\.html)!mi', '$1[topic original="$2"]$4[/topic]', $text);
 		return $text;
 	}
 
 	function text($text)
 	{
 		return $text;
+	}
+
+	function __unit_test($suite)
+	{
 	}
 }
--- classes/lcml/tag/pair/topic.php
+++ classes/lcml/tag/pair/topic.php
@@ -7,13 +7,38 @@ class lcml_tag_pair_topic extends bors_lcml_tag_pair
 		$topic_id = $content;
 		$page = defval($params, 'page');
 
+		$o = @$params['original'];
+		if(bors_exec_time() > 10)
+			return "<a href=\"$o\">$o</a>";
+
 		$topic = bors_load('balancer_board_topic', $topic_id);
+
+		if(!$topic)
+		{
+			bors_debug::syslog('forum-data-lost', "Unknown topic $topic_id");
+			return "Тема <a href=\"$o\">$o</a> не найдена";
+		}
+
 		$topic->set_page($page);
+
+
+		if(bors_exec_time() > 5)
+			return $topic->titled_link();
+
 		$first_post = $topic->first_post();
+
+		if(!$first_post)
+		{
+			bors_debug::syslog('forum-data-lost', "Unknown first post in topic {$topic} ($topic_id)");
+			$description = "";
+		}
+		else
+			$description = $first_post->snip(500);
+
 		if($params['is_alone'])
 			$html = bors_lcml_helper::box($topic, array(
 				'url' => $topic->url($page),
-				'description' => $first_post->snip(500),
+				'description' => $description,
 				'reference' => $topic->forum()->titled_link(),
 			));
 		else
