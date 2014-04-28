c691a8a81163468a467f0abd1235236ea13b4cb5
Balancer <balancer@balancer.ru>
2014-04-28 08:50:13 +0400

    Так верно.

--- engines/lcml/pre/01-airbase_links.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function lcml_airbase_links($text)
-{
-	$text = preg_replace("!http://(forums\.airbase\.ru|balancer.ru)\S+?\d{4}/\d{1,2}/t\d+\S+?\-\-\S+?#p(\d+)(\s|$| |\n)!m", 'http://www.balancer.ru/g/p$2$3', $text);
-
-	return $text;
-}
