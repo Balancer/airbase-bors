fbbe20a8788c8edbdd741ed4f5789736dc687aba
Balancer <balancer@balancer.ru>
2014-07-30 02:49:06 +0400

    Рыба, рыба... RPG-запросы готовятся.

---
+++ sites/forums.balancer.ru/classes/balancer/board/rpg/requests/edit.php
@@ -0,0 +1,7 @@
+<?php
+
+class balancer_board_rpg_requests_edit extends balancer_board_page
+{
+	var $title = 'Добавление RPG-запроса';
+	var $nav_name = 'новый';
+}
---
+++ sites/forums.balancer.ru/classes/balancer/board/rpg/requests/main.php
@@ -0,0 +1,7 @@
+<?php
+
+class balancer_board_rpg_requests_main extends balancer_board_page
+{
+	var $title = 'RPG-запросы';
+	var $nav_name = 'запросы';
+}
