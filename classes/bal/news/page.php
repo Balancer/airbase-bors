00f193dbb2d4e2429e5ebef2dfc0d607b738984e
Balancer <balancer@balancer.ru>
2015-04-25 01:55:01 +0300

    Загрузка удалённых данных.

--- classes/bal/news/page.php
+++ classes/bal/news/page.php
@@ -2,15 +2,16 @@
 
 class bal_news_page extends bors_view
 {
-	var $model_class = 'balancer_board_post';
+	var $model_class = 'bal_rpc_json_bb_post';
+	var $auto_map = true;
 
 	function url() { return '/news/'.date('Y/m/d', $this->post()->create_time()).'/'.$this->id().'.html'; }
 	function url_ex($foo) { return $this->url(); }
 
-	function auto_objects()
-	{
-		return array_merge(parent::auto_objects(), [
-			'post' => 'balancer_board_post(id)',
-		]);
-	}
+//	function auto_objects()
+//	{
+//		return array_merge(parent::auto_objects(), [
+//			'post' => 'balancer_board_post(id)',
+//		]);
+//	}
 }
---
+++ classes/bal/rpc/json/bb/post.php
@@ -0,0 +1,7 @@
+<?php
+
+class bal_rpc_json_bb_post extends bal_rpc_json_client
+{
+	var $remote_class = 'balancer_board_post';
+	var $remote_fields = 'title,body';
+}
