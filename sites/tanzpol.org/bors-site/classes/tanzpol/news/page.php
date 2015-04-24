1c245e33601a06b9e830bad6b459372db8327480
Balancer <balancer@balancer.ru>
2015-04-25 02:30:43 +0300

    Перенос кода tanzpol.org в отдельный репозиторий https://github.com/Balancer/site-tanzpol

--- sites/tanzpol.org/bors-site/classes/tanzpol/news/page.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class tanzpol_news_page extends bal_news_page
-{
-}
--- sites/tanzpol.org/bors-site/url_map.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-bors_vhost_routes('tanzpol.org', [
-	'(/news/\d{4}/\d{1,2}/\d{1,2}/)(\d+)\.html => tanzpol_news_page(2)',
-]);
