fe3ad6e8ef0750a8e4c19acff208311b24e0cfc7
Balancer <balancer@balancer.ru>
2015-04-24 14:51:02 +0300

    ++

---
+++ classes/bal/news/page.php
@@ -0,0 +1,16 @@
+<?php
+
+class bal_news_page extends bors_view
+{
+	var $model_class = 'balancer_board_post';
+
+	function url() { return '/news/'.date('Y/m/d', $this->post()->create_time()).'/'.$this->id().'.html'; }
+	function url_ex($foo) { return $this->url(); }
+
+	function auto_objects()
+	{
+		return array_merge(parent::auto_objects(), [
+			'post' => 'balancer_board_post(id)',
+		]);
+	}
+}
