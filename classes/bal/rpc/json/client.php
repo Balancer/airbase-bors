ee06bf206d8b4edaa2baa3f5e3a91c96191724c4
Balancer <balancer@balancer.ru>
2015-04-25 02:59:27 +0300

    ++

--- classes/bal/rpc/json/client.php
+++ classes/bal/rpc/json/client.php
@@ -11,6 +11,9 @@ class bal_rpc_json_client extends bors_object
 		$url = "http://www.balancer.ru/rpc/json/load/{$this->remote_class()}/{$this->id()}/{$this->remote_fields}";
 		$data = json_decode(file_get_contents($url), true);
 
+		if(count($data) == 1 && !empty($data['REMOTE_ADDR']))
+			return bors_throw('Unknown addr '.$data['REMOTE_ADDR']);
+
 		$this->data = $data;
 
 		$this->set_is_loaded(true);
--- sites/airbase.ru/classes/airbase/news/page.php
+++ sites/airbase.ru/classes/airbase/news/page.php
@@ -2,4 +2,5 @@
 
 class airbase_news_page extends bal_news_page
 {
+	var $config_class = 'airbase_config';
 }
