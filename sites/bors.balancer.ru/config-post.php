fefe036fc4dfc876644fd75a502fa98e52ad9f03
Balancer <balancer@balancer.ru>
2015-07-09 07:34:49 +0300

    merge

--- sites/bors.balancer.ru/config-post.php
+++ sites/bors.balancer.ru/config-post.php
@@ -1,6 +1,6 @@
 <?php
 
-base_object::add_template_data('template_top_menu', array(
+bors_object::add_template_data('template_top_menu', array(
 	''			=>	'главная',
 	'downloads'	=>	'скачать',
 	'blog'		=>	'блог',
@@ -12,7 +12,7 @@ base_object::add_template_data('template_top_menu', array(
 	'http://trac.balancer.ru/bors-core/'		=>	'Trac',
 ));
 
-base_object::add_template_data('bottom_counters', 'xfile:bors/site/counters.html');
+bors_object::add_template_data('bottom_counters', 'xfile:bors/site/counters.html');
 
 //template_jquery('/jquery-local/js/jquery-1.6.2.min.js');
 //template_js_include('/jquery-local/js/jquery-ui-1.8.16.custom.min.js');
